f5c087578c0931474253beb2838fab304048ba63
TommieV <TommieV@Gmail.com>
2014-01-10 17:36:15 +0100

    Added example bidding strategy

--- MatlabDomainAnalysisTools/basmeuk.m
+++ MatlabDomainAnalysisTools/basmeuk.m
@@ -1,16 +1,49 @@
 % Read Party domain xml files:
 clear
-[v1, w1, issueNames, itemNames] = importUtilSpace('../genius/etc/templates/partydomain/party_utility_kh.xml');
-[v2, w2, ~, ~] = importUtilSpace('../genius/etc/templates/partydomain/party_utility_cath.xml');
+[v1, w1, issueNames, itemNames] = importUtilSpace('../genius/etc/templates/partydomain/AI2009/party_user10_utility.xml');
+[v2, w2, ~, ~] = importUtilSpace('../genius/etc/templates/partydomain/AI2009/party_user15_utility.xml');
 
 [v, w] = mergeUtilSpaces(v1, w1, v2, w2);
 v = normalizeValues(v);
 
 % Calculate all utilities/bids
 [bs, bids] = biddingSpace(v, w);
-
+%%
+firstpar = 1;
+paretoB = [];
+for i = 1:size(bs,2)
+    curbid = bs(:,i);
+    pareto = 1;
+    %fprintf('Current bid: ');
+    %curbid
+    for j = 1:size(bs,2)
+        anotherbid = bs(:,j);
+        %anotherbid
+        if sum(anotherbid > curbid)==2
+            %fprintf('This is not pareto optimal\n')
+            pareto = 0;
+        end
+    end
+    if pareto
+        if firstpar
+            paretoB = curbid;
+            firstpar = 0;
+        else
+            paretoB = [paretoB curbid];
+        end
+    end
+end
+paretoB = sortrows(paretoB',1)
+paretoB = paretoB'
+%%
+figure
 plot(bs(1,:), bs(2,:), '.'); hold on;
-
+plot(paretoB(1,:), paretoB(2,:), '.-r')
+title('Pareto frontier')
+xlabel('Party Utility 10')
+ylabel('Party Utility 15')
+print(gcf,'-dpng','-r300','pareto.png');
+%%
 % Calculate and show the nash point
 [ns I] = nash(bs);
 optItems = bids(:, I);
