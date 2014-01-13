a1ca4a7175db0c46926e976bfedc6c2d8203121b
ASchutter <aschutter@gmail.com>
2014-01-13 19:53:26 +0100

    Nothing

--- NegotiationAgent/src/negotiator/group7/Group7_BS.java
+++ NegotiationAgent/src/negotiator/group7/Group7_BS.java
@@ -211,7 +211,6 @@ public class Group7_BS extends OfferingStrategy {
 				Double temp = new Double(nextBidUtil);
 				Double range2 = new Double(phase2range);
 
-
 				Log.vln("I want an utility of: " + temp.toString() + " range: " + range2);
 
 				List<BidDetails> bidsInRange = negotiationSession.getOutcomeSpace().getBidsinRange(r);
