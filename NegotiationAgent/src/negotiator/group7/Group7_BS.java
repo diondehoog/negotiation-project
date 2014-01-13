7eb43f061e7b661fee9316d10e2e8f2c164fa2d1
immortaly007 <immortaly007@gmail.com>
2014-01-13 17:01:28 +0100

    Changed phase1 duration to 0.2

--- NegotiationAgent/src/negotiator/group7/Group7_BS.java
+++ NegotiationAgent/src/negotiator/group7/Group7_BS.java
@@ -42,7 +42,7 @@ public class Group7_BS extends OfferingStrategy {
 	SortedOutcomeSpace outcomespace;
 	
 	/** Phase boundaries */
-	private double[] phaseBoundary = {0.0, 0.8};
+	private double[] phaseBoundary = {0.2, 0.8};
 	private double   phase1LowerBound = 0.8;
 	private double   phase1UpperBound = 1.0;
 	private double   phase2LowerBound = 0.6;
