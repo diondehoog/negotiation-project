4a90acf7a298ddbd54ed3ebb7aff42fd55b15e56
Tom Runia <tomrunia@gmail.com>
2014-01-13 15:20:08 +0100

    Added missing library

--- NegotiationAgent/src/negotiator/group7/Group7_BS.java
+++ NegotiationAgent/src/negotiator/group7/Group7_BS.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Random;
 
 import misc.Range;
+import negotiator.BidHistory;
 import negotiator.bidding.BidDetails;
 import negotiator.boaframework.NegotiationSession;
 import negotiator.boaframework.OMStrategy;
