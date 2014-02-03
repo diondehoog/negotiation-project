00286268f0f070e737600daf264787e96924e79c
Olivier Hokke <github@wolfox.nl>
2014-02-03 15:41:36 +0100

    Added comments and fallback (which is a random bid out of our history)

--- NegotiationAgent/src/negotiator/group7/phases/Phase3.java
+++ NegotiationAgent/src/negotiator/group7/phases/Phase3.java
@@ -23,35 +23,53 @@ public class Phase3 extends Phase {
 	public BidDetails determineNextBid() {
 		
 		int opponentStrategy = ourHelper.getOMStrategy().getOpponentModel();
+
+		Random randgen = new Random();
+		BidDetails fallback;
+		List<BidDetails> bh = negotiationSession.getOwnBidHistory().getHistory();
+		fallback = bh.get(randgen.nextInt(bh.size()));
 		
-		if (opponentStrategy == 1) {
-			// Opponent is assumed to be HardHeaded
-			Log.rln("Opponent is assumed to be HardHeaded, offering random bid [0.7, 0.8]");
+		if (opponentStrategy == 1) // Opponent is assumed to be HardHeaded
+		{
+			
+			Log.rln("Opponent is assumed to be HardHeaded, decreasingly offering random bid that approaches the pareto");
 
 			BidDetails best = null;
-			Random randgen = new Random();
 			double time = (negotiationSession.getTime() - phaseStart) * (1 / (1 - phaseStart));
+			int tries = 15;
 			
-			while (best == null) {
+			while (best == null && tries > 0) 
+			{
 				double u = randgen.nextDouble() * 0.1 + 0.7 + 0.2 * (1 - time);
-				Log.hln("  u: " + u);
 				Range r = new Range(u - 0.01, u + 0.01);
 				List<BidDetails> randBid = negotiationSession.getOutcomeSpace().getBidsinRange(r);
+				
 				double bestValue = 0.0;
-				for (BidDetails b : randBid) {
+				for (BidDetails b : randBid) 
+				{
 					double value = ourHelper.getOpponentModel().getBidEvaluation(b.getBid());
-					if (value > bestValue) {
+					if (value > bestValue) 
+					{
 						best = b;
 						bestValue = value;
 					}
 				}
+				tries--;
 			}
 			
-			return best;
-		} else {
+			if (best == null)
+				return fallback;
+			else
+				return best;
+		} 
+		else 
+		{
 			// Opponent is assumed to be Conceder
 			Log.rln("Opponent is assumed to be Conceder, offering KS point");
-			return ourHelper.getKalaiPoint();
+			if (ourHelper.getKalaiPoint() != null)
+				return ourHelper.getKalaiPoint();
+			else
+				return fallback;
 		}
 		
 	}
