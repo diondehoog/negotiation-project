d8628326dd7016ec52dfb3ea74e92b40cc25973b
immortaly007 <immortaly007@gmail.com>
2014-02-04 03:45:36 +0100

    Removed some unused imports + added method names to report

--- NegotiationAgent/src/negotiator/group7/Group7_FrequencyOM.java
+++ NegotiationAgent/src/negotiator/group7/Group7_FrequencyOM.java
@@ -1,12 +1,8 @@
 package negotiator.group7;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import negotiator.Bid;
